42994fccce2b4bc2f1a4a9948faafa71735f15c0
paulacata <paula.cata01@estudiant.upf.edu>
2022-05-05 01:44:12 +0200

    Practica 2

---
+++ Sheep Rescue/Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float movementSpeed;
+    public float horizontalBoundary = 22;
+    public GameObject hayBalePrefab; //Reference to the Hay Bale prefab
+    public Transform haySpawnpoint; //The point from which the hay will be shot
+    public float shootInterval; //The smallest amount of time between shots
+    private float shootTimer; //A timer to keep track whether the machine can shoot or not
+
+    public Transform modelParent;
+    public GameObject blueModelPrefab;
+    public GameObject yellowModelPrefab;
+    public GameObject redModelPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadModel();
+    }
+
+    private void LoadModel()
+    {
+        Destroy(modelParent.GetChild(0).gameObject);
+
+        switch (GameSettings.hayMachineColor)
+        {
+            case HayMachineColor.Blue:
+                Instantiate(blueModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Yellow:
+                Instantiate(yellowModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Red:
+                Instantiate(redModelPrefab, modelParent);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) //left key
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) //right key
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+        SoundManager.Instance.PlayShootClip();
+    }
+}
